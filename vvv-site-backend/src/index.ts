0b5e9e9524891555a7d8a1d262ab7541b9799637
jackveith <jackveith28@gmail.com>
2025-10-11 03:21:57 -0400

    clean index.ts for prisma init

--- vvv-site-backend/src/index.ts
+++ vvv-site-backend/src/index.ts
@@ -4,28 +4,10 @@ import { MongoClient, ServerApiVersion, Db } from "mongodb";
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma_db_url = `${process.env.DATABASE_URL}`;
 
 const app = express();
-const c = 0;
-
-/*
-const uri = `${process.env.DATABASE_URI}`;
-let db: Db;
-
-async function connectDB() {
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    await client.connect();
-    db = client.db("vvv-fullstack-db-main");
-    console.log("Connected to MongoDB.");
-}
-*/
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 // Middleware
 app.use(cors());
@@ -36,36 +18,3 @@ app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from Express!" });
 });
 
-
-app.get("/ping", (req, res) => {
-    res.json({ message: "ping test succeeded. And, good day :)" });
-});
-
-
-app.get("/user/:name", async (req, res) => {
-
-    try {
-        const name = req.params.name;
-
-        const result = await db
-            .collection("Users")
-            .findOne({ name: name });
-
-        if (!result) {
-            return res.status(404).json({ message: "User not found." });
-        }
-
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Internal server error." });
-    }
-});
-
-
-// Start server
-connectDB().then(() => {
-    app.listen(4000, () => {
-        console.log("API server running at http://localhost:4000");
-    });
-});
